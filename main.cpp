4a3e30a3945e96080acbf95e98a1447605876c1a
Zion Ohonsi <oseiweohonsi@gmail.com>
2024-04-05 16:57:17 -0400

    created the main with variables

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Morse code translator
+// Note: The letters of a word are separated by a space of duration equal to three dits,
+// and words are separated by a space equal to seven dits
+#include <iostream>
+#include <fstream> // read from files
+using namespace std;
+int main()
+{
+  // open file of morse code characters
+  ifstream file("code.txt");
+  string line; // created to store each string
+  char letter; // stores latin alphabets
+  vector<string> alphabets;
+  vector<char> morse_code_chars; 
+  return 0;
+}
