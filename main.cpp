52a4a9e4b4d93c0b0c05f13cf6dcfc8285604a95
Zion Ohonsi <oseiweohonsi@gmail.com>
2024-04-06 19:45:47 -0400

    closed the file

--- main.cpp
+++ main.cpp
@@ -3,14 +3,33 @@
 // and words are separated by a space equal to seven dits
 #include <iostream>
 #include <fstream> // read from files
+#include <vector> // vector headers
 using namespace std;
 int main()
 {
-  // open file of morse code characters
-  ifstream file("code.txt");
+ 
   string line; // created to store each string
   char letter; // stores latin alphabets
-  vector<string> alphabets;
-  vector<char> morse_code_chars; 
+  vector<char> alphabets; // store all the alphabets
+  vector<string> morse_code_chars; // store all the morse code
+
+  ifstream file("code.txt");
+  while(file)
+    {
+      file >> letter >> line;
+      alphabets.push_back(letter);
+      morse_code_chars.push_back(line);
+    }
+  for (char val: alphabets)
+    {
+      cout << val << endl;
+    }
+  for (string val: morse_code_chars)
+    {
+      cout << val << endl;
+    }
+  file.close()
+  
+  
   return 0;
 }
