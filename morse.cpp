fee4e25bd4593784f9ae891357408e268b3cf238
zion ohonsi <oseiweohonsi@gmail.com>
2024-04-07 22:28:25 -0400

    added necessary function

--- morse.cpp
+++ morse.cpp
@@ -1,7 +1,16 @@
-// morse code translator
+// Morse code translator
+// Note: The letters of a word are separated by a space of duration equal to three dits,
 #include <iostream>
+#include <string>
 using namespace std;
-int main()
+class Morse
 {
-    return 0;
+    private:
+        string message;
+    public:
+        void convertToMorse();
+};
+
+void Morse::convertToMorse()
+{
 }
