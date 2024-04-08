9cf3856ccc1c544a423b247f7c22260bdf2309f4
zion ohonsi <oseiweohonsi@gmail.com>
2024-04-08 14:01:23 -0400

    completed the necessary private and public variables

--- Morse.h
+++ Morse.h
@@ -15,26 +15,6 @@ private:
     vector<string> morse_code; // store the morse code
 public:
     void convertToMorse(string);
-    void getCharacters(vector<char>, vector<string>); // get the characters from the file
+    void getCharacters(vector<char> &, vector<string> &); // get the characters from the file
 };
-
-void Morse::getCharacters(vector<char> letters, vector<string> code)
-{
-    // open the file
-    ifstream file;
-    file.open("code.txt");
-    // temporarily store file contents
-    char temp_letter;
-    string temp_code;
-    if (file)
-    {
-        while (file >> temp_letter >> temp_code)
-        {
-            letters.push_back(temp_letter);
-            code.push_back(temp_code);
-        }
-    }
-    // print the characters
-    file.close();
-}
 #endif
